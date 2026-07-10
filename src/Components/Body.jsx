f939b691de0d5916820c130878104503517ec96d
Piyush Satyam <piyushsatya81998@gmail.com>
2026-07-11 01:13:51 +0530

    useLocation to avoid unnecessary API calss without LogIn/SignUp

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,27 +1,28 @@
-import React, {useEffect} from 'react'
-import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom';
-import Footer from './Footer';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { BASE_URL } from '../utils/constants';
-import { addUser } from '../utils/userSlice';
-
+import React, { useEffect } from "react";
+import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../utils/userSlice";
 
 export const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const location = useLocation();
   const userData = useSelector((state) => state.user);
 
   const fetchUserData = async () => {
-    if (userData) {
+    if (userData || location.pathname === "/login") {
       return;
     }
     try {
-      const res = await axios.get(BASE_URL + "/profile/view", { withCredentials: true });
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
       dispatch(addUser(res.data));
-
     } catch (error) {
       if (error.response?.status === 401) {
         navigate("/login");
@@ -36,11 +37,11 @@ export const Body = () => {
 
   return (
     <div>
-        <NavBar/>
-        <Outlet/>
-        <Footer/>
+      <NavBar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Body;
